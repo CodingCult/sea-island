6c398975c7b407715435a135c7899b3432527f08
Eric Xiao <ericxiao0000@gmail.com>
2024-08-26 11:02:14 -0700

    feat(lint): add ESLint configuration

---
+++ client/eslint.config.js
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            // Ignore unused variables that start with an underscore (_)
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    varsIgnorePattern: "^_",
+                    argsIgnorePattern: "^_",
+                    caughtErrorsIgnorePattern: "^_",
+                },
+            ],
+        },
+    },
+    { ignores: ["dist/"] },
+];
