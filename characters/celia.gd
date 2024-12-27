e3e328bdb6e072c74de8936f8b7ceb0a93682857
Eric Xiao <ericxiao0000@gmail.com>
2024-12-26 19:45:22 -0800

    feat: remove fast falling

--- characters/celia.gd
+++ characters/celia.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY := -400.0
 
 @onready var animated_sprite := $AnimatedSprite2D
 var jumped := false
-var fast_falling := false
+# var fast_falling := false
 
 func _process(_delta):
     animated_sprite.play("default")
@@ -20,17 +20,17 @@ func _process(_delta):
 func _physics_process(delta):
     # Add the gravity.
     if is_on_floor():
-        fast_falling = false
+        # fast_falling = false
         jumped = false
     else:
         velocity += get_gravity() * delta
     
     # Fast fall to give player more control
-    if Input.is_action_just_released("jump") and jumped:
-        fast_falling = true
+    # if Input.is_action_just_released("jump") and jumped:
+    #     fast_falling = true
     
-    if fast_falling:
-        velocity += get_gravity() * delta * 0.75
+    # if fast_falling:
+    #     velocity += get_gravity() * delta * 0.75
 
     var direction := Input.get_axis("left", "right")
     if direction:
@@ -44,7 +44,7 @@ func _physics_process(delta):
             if Input.is_action_pressed("jump"):
                 velocity.y = JUMP_VELOCITY
             else:
-                velocity.y = JUMP_VELOCITY * 0.5
+                velocity.y = JUMP_VELOCITY * 0.25
     else:
         velocity.x = move_toward(velocity.x, 0, SPEED) # Stops in 0s
         # velocity.x = move_toward(velocity.x, 0, delta * SPEED / 0.2) # Stops in 0.2s
