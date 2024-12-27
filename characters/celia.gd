684d360782fcc1049befaa1417dd9cd88d6e4262
Eric Xiao <ericxiao0000@gmail.com>
2024-12-26 23:19:35 -0800

    fix: slight adjustments to celia movement

--- characters/celia.gd
+++ characters/celia.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED := 300.0
+const SPEED := 200.0
 const JUMP_VELOCITY := -400.0
 
 @onready var animated_sprite := $AnimatedSprite2D
@@ -27,30 +27,28 @@ func _physics_process(delta):
     else:
         velocity += get_gravity() * delta
 
-    if is_on_wall_only():
+    if is_on_wall_only() and jumped:
         var jump_direction := get_slide_collision(0).get_normal().x
 
-        velocity.y = JUMP_VELOCITY * 0.75
+        velocity.y = JUMP_VELOCITY
         velocity.x = jump_direction * SPEED * 0.5
-        jump_wait = 0.1
+        jump_wait = 0.2
 
     jump_wait -= delta
 
     var direction := Input.get_axis("left", "right")
     if direction and jump_wait <= 0:
-        velocity.x = direction * SPEED
+        velocity.x = SPEED * direction
 
         # Auto jumps if Celia is moving horizontally
         if is_on_floor():
-            jumped = true
-
             # Jump higher if holding jump
             if Input.is_action_pressed("jump"):
                 velocity.y = JUMP_VELOCITY
+                jumped = true
             else:
                 velocity.y = JUMP_VELOCITY * 0.25
     elif jump_wait <= 0:
-        velocity.x = move_toward(velocity.x, 0, SPEED) # Stops in 0s
-        # velocity.x = move_toward(velocity.x, 0, delta * SPEED / 0.2) # Stops in 0.2s
+        velocity.x = 0
 
     move_and_slide()
