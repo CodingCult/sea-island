9434c9f79ae9b89fd88f9130a627d0bb5159db31
Eric Xiao <ericxiao0000@gmail.com>
2024-12-31 18:17:01 -0800

    feat(camera): adjustments to camera following algorithm
    - Camera moves faster when farther away, prevents waiting for camera to
      follow
    - Camera cannot not exceed Celia's velocity

--- misc/camera.gd
+++ misc/camera.gd
@@ -8,6 +8,13 @@ const SPEED := 128.0
 func _process(delta: float) -> void:
     # Moves to discrete height levels
     var height_level = int(celia.position.y / 32)
-    global_position.y = move_toward(global_position.y, height_level * 32, SPEED * delta)
+    
+    # Moves faster if farther away
+    # FIX: if camera moves too fast when following, celia looks to be stuttering
+    var distance_difference = abs(celia.position.y - global_position.y)
+    
+    var speed_constant = clamp(distance_difference / 32, 1, 2)
+    
+    global_position.y = move_toward(global_position.y, height_level * 32, SPEED * speed_constant * delta)
 
     global_position.x = celia.position.x
