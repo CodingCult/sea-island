9bd8af83abcab81834566e52b8e7c270093a0fb5
Eric Xiao <ericxiao0000@gmail.com>
2024-12-31 18:05:51 -0800

    fix: minor godot warning fixes

--- stages/main_manager.gd
+++ stages/main_manager.gd
@@ -1,4 +1,4 @@
 extends Node
 
 func change_scene_pond() -> void:
-    get_tree().change_scene_to_file("res://stages/pond.tscn")
+    get_tree().change_scene_to_file.bind("res://stages/pond.tscn").call_deferred()
