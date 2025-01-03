5abb034e2ffd5c2e877598efe527354370444ed2
Eric Xiao <ericxiao0000@gmail.com>
2025-01-03 15:05:35 -0800

    refactor(ui): move ui scenes to ui/ folder

--- entities/characters/celia/celia.gd
+++ entities/characters/celia/celia.gd
@@ -5,7 +5,8 @@ const SPEED := 200.0
 const JUMP_VELOCITY := -400.0
 
 @onready var animated_sprite := $AnimatedSprite2D
-@onready var debug_label: Label = %DEBUG
+@onready var game_ui: CanvasLayer = %GameUI
+@onready var debug_text: Label = game_ui.get_node("DebugText")
 
 var jumped := false
 var jump_wait := 0.0
@@ -86,7 +87,7 @@ func get_colliding_tiles(collision: KinematicCollision2D) -> Array[TileData]:
 
 
 func handle_damage_collisions():
-    debug_label.text = ""
+    debug_text.text = ""
     for i in range(get_slide_collision_count()):
         var collision := get_slide_collision(i)
         var collider := collision.get_collider()
