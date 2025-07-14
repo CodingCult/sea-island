eefa7adf97e55177fa92d1cd50adcbbb9d1c1fb8
Eric Xiao <ericxiao0000@gmail.com>
2025-07-14 11:42:47 -0700

    feat: add fullscreen toggle

--- config/config.gd
+++ config/config.gd
@@ -1,26 +1,40 @@
 extends Node
 
 
+func _ready() -> void:
+	apply_config(get_config())
+
+
+func apply_config(config: ConfigFile):
+	var is_fullscreen: bool = config.get_value("video", "fullscreen", true)
+
+	if is_fullscreen:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		DisplayServer.window_set_size(Vector2i(1920, 1080))
+
+
 func write_config(config: ConfigFile):
-    config.save("user://config.ini");
+	config.save("user://config.ini");
 
 
 func get_default_config() -> ConfigFile:
-    var config = ConfigFile.new()
-    var err = config.load("res://config/default_config.ini")
+	var config = ConfigFile.new()
+	var err = config.load("res://config/default_config.ini")
 
-    assert(err == OK, "Could not load default_config.ini")
+	assert(err == OK, "Could not load default_config.ini")
 
-    return config
+	return config
 
 
 func get_config() -> ConfigFile:
-    var config = ConfigFile.new()
-    var err = config.load("user://config.ini")
+	var config = ConfigFile.new()
+	var err = config.load("user://config.ini")
 
-    if err != OK:
-        var default_config = get_default_config()
-        write_config(default_config)
-        return default_config
-    
-    return config
+	if err != OK:
+		var default_config = get_default_config()
+		write_config(default_config)
+		return default_config
+	
+	return config
--- ui/menus/settings_menu.gd
+++ ui/menus/settings_menu.gd
@@ -1,5 +1,12 @@
 extends Control
 
+@onready var sound_on_button: CheckButton = $MarginContainer/VBoxContainer/SoundToggle
+@onready var fullscreen_button: CheckButton = $MarginContainer/VBoxContainer/FullscreenToggle
+
+func _ready() -> void:
+	var config = Config.get_config()
+	sound_on_button.button_pressed = config.get_value("sound", "on", false)
+	fullscreen_button.button_pressed = config.get_value("video", "fullscreen", false)
 
 func _on_return_button_pressed() -> void:
 	Utils.change_stage(Utils.Stage.MAIN_MENU)
@@ -9,3 +16,11 @@ func _on_check_button_toggled(toggled_on: bool) -> void:
 	var config = Config.get_config()
 	config.set_value("sound", "on", toggled_on)
 	Config.write_config(config)
+	Config.apply_config(config)
+
+
+func _on_check_button_2_toggled(toggled_on: bool) -> void:
+	var config = Config.get_config()
+	config.set_value("video", "fullscreen", toggled_on)
+	Config.write_config(config)
+	Config.apply_config(config)
